38002b71243b05623093e9912ed5dd0883d42cbd
zhaocheng <zhaocheng@meixing.com>
2024-02-05 15:48:56 +0800

    成员函数指针

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-#include "defaultParam.h"
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    void func();
+    virtual void virFunc();
+    int ptr;
+};
+
+void A::func() {
+
+}
+
+void A::virFunc() {
+
+}
+
 
 int main()
 {
-    f();
+    auto basePtr = &A::ptr;
+    cout << *reinterpret_cast<int*>(&basePtr) << "\n";
+
+    auto VirPtr = &A::virFunc;
+    auto func = *(int*)(&VirPtr);
+    std::cout<< func <<'\n';
+
+    auto funcPtr = &A::func;
+    cout << funcPtr << "\n";
     return 0;
 }
