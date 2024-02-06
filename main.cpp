334f7a01afcdc8c7d3dfc970da021ed1965dffd6
zhaocheng <zhaocheng@meixing.com>
2024-02-06 11:06:56 +0800

    [变更内容] 值类别 [组件] 无 [影响范围] 无 [限制事项] 无 [CodeReview] zhaocheng

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 
 
+
 int main()
 {
     auto p = [num = 0](){return 0;};
     auto q = [](){return 0;};
-    auto p_ = []<typename T>(T a){cout << a << "\n";};
+    auto p_ = [](auto a){cout << a << "\n";};
     p_(10);
     p_("str");
     cout << sizeof p << "\n";  // 4
     cout << sizeof q << "\n";  // 1
     return 0;
 }
+
