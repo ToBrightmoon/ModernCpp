b05a87563109b86c9dd1b9bfc569add409441041
ToBrightMoon <1870035544@qq.com>
2024-02-09 16:16:17 +0800

    形参包名

--- main.cpp
+++ main.cpp
@@ -2,21 +2,31 @@
 #include <vector>
 using namespace std;
 
-template <class T>
-struct X {
-    T x;
-};
 
-template <class T , template<class Elem> class C = X>
-        struct TemplateN
-        {
-            T y;
-            X<T> x;
-        };
+//template <typename ...Args>
+//void Print(Args... args)
+//{
+//    Print(args...);
+//}
+
+template <typename T>
+void Print(T a)
+{
+    cout << a << "\n";
+}
+
+template <typename T,typename ...Args>
+void Print(T a, Args... args)
+{
+    cout << a << " ";
+    Print(args...);
+}
+
+
 
 int main()
 {
-    TemplateN<int,X> a{};
+    Print(1,2,3,4,5);
     return 0;
 }
 
